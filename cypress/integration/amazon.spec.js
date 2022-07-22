dc6184cd3809e68b697f188f0fe488ffe255d877
juampicres <jpablo.cres@gmail.com>
2022-07-22 15:34:17 -0300

    comienzo amazon

--- cypress/integration/amazon.spec.js
+++ cypress/integration/amazon.spec.js
@@ -1,14 +1,18 @@
 /// <reference types="cypress" />
 
 
-    context('Buscando en NIKE con el campo de busqueda', () => {    
+    context('Buscando en AMAZON USA', () => {    
       beforeEach(() => {
         cy.visit('www.amazon.com')
         cy.url().should('include', 'https://www.amazon.com') //verificamos que estamos en la pagina de nike
       })
   
-      it('Buscar un producto en Amazon USA', () => {
-        
+      it('Buscar un producto', () => {
+        cy.contains('Deliver to').click()  //obtenemos el input de busqueda
+        cy.contains("CAMBIAR LA DIRECCIÓN")
+          .click()
+        cy.wait(5000)
+        cy.scrollTo('top')
       })
   
   })
