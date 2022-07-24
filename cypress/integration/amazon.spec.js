f666f8141b6afaa86be74103a2dcb2574815fb26
juampicres <jpablo.cres@gmail.com>
2022-07-24 12:11:12 -0300

    se tipea algo en criterio busqueda

--- cypress/integration/amazon.spec.js
+++ cypress/integration/amazon.spec.js
@@ -8,26 +8,28 @@
       })
   
       it('Buscar un producto', () => {
-        cy.wait(5000)
-        cy.get('#nav-main > div.nav-left > div > div > div.glow-toaster-footer > span.a-button.a-spacing-top-base.a-button-primary.glow-toaster-button.glow-toaster-button-submit > span > input').click()
-       
-        //cy.contains('Deliver to').click()  //obtenemos el input de busqueda
-        //cy.contains("CAMBIAR LA DIRECCIÓN")
-          //.click()
-        //cy.scrollTo('top')
-
+        cy.get('[data-action-type="SELECT_LOCATION"]')
+          .click()
+        cy.contains("Choose your location");
         cy.get('#GLUXZipUpdateInput')
-          .type('33166')
-        cy.contains('Aplicar').click({force: true}) 
+          .type('33166') //buscamos el input de zipCode
+        cy.contains('Aplicar').click({ force: true }) //click en aplicar
+        cy.wait(1000) //esperamos 1 segundos
+        cy.intercept('GET', 'https://www.amazon.com/portal-migration/hz/glow/get-location-label*').as('callZipChange')
+        cy.get('.a-popover-footer').within(()=>{
+          cy.get('.a-button').click()
+        })
+        
+        cy.wait('@callZipChange').then((xhr) => {
+          //revisamos que el wrapper del zipCode se haya cambiado
+          cy.get('#nav-global-location-popover-link').within(()=>{
+            cy.contains("Miami 33166")
+          })
+        })
 
-        cy.get('#a-popover-1 > div').within(()=>{
-          cy.contains('Continuar')
-       })
-
-        //cy.contains('Continuar').click({force: true})
-
+        cy.get('#twotabsearchtextbox').scrollIntoView()
+          .type('Anta KT 4{enter}') //buscamos algo
       })
-  
   })
 
   
